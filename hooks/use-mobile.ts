305692b61339080044b2280b65bcc7df322f089c
alessandrododi <dodialessandro3@gmail.com>
2026-02-07 10:07:21 +0100

    fix: improve useIsMobile hook to reduce hydration mismatches
    Changed from !!isMobile to isMobile ?? false for clearer SSR handling

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -15,5 +15,7 @@ export function useIsMobile() {
     return () => mql.removeEventListener("change", onChange)
   }, [])
 
-  return !!isMobile
+  // Return false during SSR/hydration to avoid mismatch
+  // Will update to true after hydration if viewport is mobile
+  return isMobile ?? false
 }
